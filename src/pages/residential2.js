33fbbbdf837e10f444b6c3d2fcac1b2af99c4ef1
GregPlaya <110426691+GregPlaya@users.noreply.github.com>
2022-09-14 11:26:02 -0700

    Update residential2.js

--- src/pages/residential2.js
+++ src/pages/residential2.js
@@ -86,13 +86,13 @@ const ResidentialPage = () => {
       </section>
       <section className="full">
         <div className="row bg-orange color-white align-center d-flex align-items-center justify-content-center" style={{ minHeight: "50vh" }}>
-           <div className="col-12 col-md-3 col-lg-3">
-            <StaticImage src="../images/sandbox-solana-beach-residential-living-leasing-Unit-Type-A.jpg" alt="" />
+           <div className="col-12 col-md-3 col-lg-3 px-2">
+             <StaticImage src="../images/sandbox-solana-beach-residential-living-leasing-Unit-Type-A.jpg" alt="" />
              <div className="short-desc caps font-com font-bold my-40">
               Unit Type A
              </div>
              <hr className="bg-white my-30 mx-5" />
-             <div className="short-desc font-com">
+             <div className="short-desc font-com font-20">
               7 - 1 bedroom units
               <span className="d-none d-sm-inline">
                 &nbsp;&nbsp;|&nbsp;&nbsp;
