ff6b2bfd3f719be7b959a8c07e5efbb7c39a4ec2
GregPlaya <greg@playacp.com>
2022-09-20 17:21:58 -0700

    Remove Simple React Lightbox from gastby-browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,12 +8,3 @@
 
 //import React from 'react';
 import "bootstrap/dist/css/bootstrap-grid.min.css"
-import React from 'react'
-import SimpleReactLightbox from 'simple-react-lightbox'
-// USE THE IMPORT BELOW INSTEAD IF YOU ARE USING THE PRO VERSION
-// import SimpleReactLightbox from 'simple-react-lightbox-pro'
-
-// eslint-disable-next-line react/prop-types
-export const wrapRootElement = ({ element }) => (
-  <SimpleReactLightbox>{element}</SimpleReactLightbox>
-)
