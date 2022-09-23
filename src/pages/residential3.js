cab0fbbc16e7e61bce50771d9bb865e768230499
GregPlaya <110426691+GregPlaya@users.noreply.github.com>
2022-09-23 16:46:47 -0700

    Residential 3 add lightbox

--- src/pages/residential3.js
+++ src/pages/residential3.js
@@ -8,148 +8,49 @@ import Seo from "../components/seo"
 import "../styles/residential.css"
 import SubscribeFormRes from "../components/mailchimp-form-res"
 
-const ResidentialPage = () => {
-  return (
-    <Layout>
-      <Seo title="Sandbox Solana - Residential" />
-      <section className="full">
-        <div style={{ display: "grid" }}>
-          <StaticImage
-            style={{
-              gridArea: "1/1",
-            }}
-            layout="fullWidth"
-            alt=""
-            src={"../images/sierra-ave-2.jpg"}
-            formats={["auto", "webp", "avif"]}
-          />
-          <div
-            style={{
-              gridArea: "1/1",
-              position: "relative",
-              justifyContent: "center",
-              display: "grid",
-            }}
-            className="residential-logo-wrapper"
-          >
-            <Link to="/" style={{ padding: "30px" }}>
-              <img
-                className="img-fluid logo"
-                src={"/logo-white.svg"}
-                alt="Sandbox"
-              />
-            </Link>
-          </div>
-        </div>
-      </section>
-      <section className="full">
-        <div
-          className="bg-orange color-white align-center d-flex align-items-center justify-content-center"
-          style={{ minHeight: "50vh" }}
-        >
-          <div className="desc-box">
-            <div className="short-desc font-com mb-100">
-              Welcome to your Solana Beach home. West of the 101 and just steps
-              from the beach, Sandbox offers residents modern, integrated living
-              spaces in a local neighborhood atmosphere.
-            </div>
-            <div className="short-desc caps font-com font-bold">
-              Newly constructed modern living in highly desirable coastal
-              community
-            </div>
-            <hr className="bg-white my-40" />
-            <div className="short-desc caps font-com font-bold">
-              7 - 1 bedroom units
-              <span className="d-none d-sm-inline">
-                &nbsp;&nbsp;|&nbsp;&nbsp;
-              </span>
-              <br className="d-sm-none" />
-              18 - 2 bedroom units
-            </div>
-          </div>
-        </div>
-      </section>
-      <section className="">
-        <div className="d-md-flex">
-          <div className="col-12 col-md-6">
-            <StaticImage src="../images/yellow-chair-1.png" alt="" />
-          </div>
-          <div className="col-12 col-md-6 d-flex align-items-center justify-content-center">
-            <div className="desc-box">
-              <div className="short-desc font-com color-blue align-center">
-                Want to <br /> live at Sandbox?
-                <SubscribeFormRes />
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section className="full">
-        <div className="row bg-orange color-white align-center d-flex align-items-center justify-content-center px-2 res-grid" style={{ minHeight: "50vh" }}>
-           <div className="col-12 col-md-3 col-lg-3">
-             <StaticImage src="../images/sandbox-solana-beach-residential-living-leasing-Unit-Type-A.jpg" alt=""/>
-             <div className="short-desc caps font-com font-bold mt-50">
-              Unit Type A
-             </div>
-             <hr className="bg-white my-20 mx-5" />
-             <div className="short-desc font-com font-20">
-              7 - 1 bedroom units
-              <span className="d-none d-sm-inline">
-                &nbsp;&nbsp;|&nbsp;&nbsp;
-              </span>
-              <br className="d-sm-none" />
-              2 Bed + 2 Bath 1,220 sf
-              </div>
-          </div>
-<div className="col-12 col-md-3 col-lg-3">
-             <StaticImage src="../images/sandbox-solana-beach-residential-living-leasing-Unit-Type-A.jpg" alt=""/>
-             <div className="short-desc caps font-com font-bold mt-50">
-              Unit Type A
-             </div>
-             <hr className="bg-white my-20 mx-5" />
-             <div className="short-desc font-com font-20">
-              7 - 1 bedroom units
-              <span className="d-none d-sm-inline">
-                &nbsp;&nbsp;|&nbsp;&nbsp;
-              </span>
-              <br className="d-sm-none" />
-              2 Bed + 2 Bath 1,220 sf
-              </div>
-          </div>
-<div className="col-12 col-md-3 col-lg-3">
-             <StaticImage src="../images/sandbox-solana-beach-residential-living-leasing-Unit-Type-A.jpg" alt=""/>
-             <div className="short-desc caps font-com font-bold mt-50">
-              Unit Type A
-             </div>
-             <hr className="bg-white my-20 mx-5" />
-             <div className="short-desc font-com font-20">
-              7 - 1 bedroom units
-              <span className="d-none d-sm-inline">
-                &nbsp;&nbsp;|&nbsp;&nbsp;
-              </span>
-              <br className="d-sm-none" />
-              2 Bed + 2 Bath 1,220 sf
-              </div>
-          </div>
-<div className="col-12 col-md-3 col-lg-3">
-             <StaticImage src="../images/sandbox-solana-beach-residential-living-leasing-Unit-Type-A.jpg" alt=""/>
-             <div className="short-desc caps font-com font-bold mt-50">
-              Unit Type A
-             </div>
-             <hr className="bg-white my-20 mx-5" />
-             <div className="short-desc font-com font-20">
-              7 - 1 bedroom units
-              <span className="d-none d-sm-inline">
-                &nbsp;&nbsp;|&nbsp;&nbsp;
-              </span>
-              <br className="d-sm-none" />
-              2 Bed + 2 Bath 1,220 sf
-              </div>
-          </div>
-       </div>
-      </section>
-    </Layout>
-  )
-}
+const LightBox = ({ children, src, alt, Wrapper = 'div', zIndex = 100 }) => {
+ 
+	const [isOpen, setIsOpen] = React.useState(false);
 
-export default ResidentialPage
+	const toggleIsOpen = () => {
+		setIsOpen(!isOpen);
+	};
+
+	return (
+		<Wrapper onClick={toggleIsOpen}>
+			{children}
+			{isOpen ?
+				<div onClick={toggleIsOpen} style={{
+					position: 'fixed',
+					top: '0',
+					left: '0',
+					height: '100vh',
+					width: '100vw',
+					backgroundColor: 'rgba(0,0,0,0.7)',
+					cursor: 'pointer',
+					zIndex
+				}}>
+					<img src={src}
+						alt={alt}
+						style={{
+							height: '100%',
+							width: 'auto'
+						}}
+					/>
+				</div>
+				: null}
+		</Wrapper>
+	);
+};
+
+const LightboxApp = function () {
+	return (<div>
+	  <h1>Lightbox App</h1>
+    <h2>My Gallery</h2>
+      <LightBox src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png" alt="React Logo">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png" width="100px" height="100px" alt="React Logo" />
+      </LightBox>
+	</div>);
+};
+
+ReactDOM.render(<LightboxApp />, document.querySelector("#app"))
