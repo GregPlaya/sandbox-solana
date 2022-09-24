a0a6d418df58340e6bbb3c19ec29fbafdec8997e
GregPlaya <110426691+GregPlaya@users.noreply.github.com>
2022-09-23 21:36:32 -0700

    Simple lightbox working

--- src/pages/simple.js
+++ src/pages/simple.js
@@ -2,6 +2,7 @@ import * as React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
+import LightBox from "../components/lightbox"
 
 const ResPage = () => {
 
@@ -11,6 +12,9 @@ const ResPage = () => {
         <div>
            <h1>Lightbox App</h1>
          <h2>My Gallery</h2>
+          <LightBox src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png" alt="React Logo">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png" width="100px" height="100px" alt="React Logo" />
+      </LightBox>
        </div>
       </section>
     </Layout>
