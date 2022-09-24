7eab7c76801762ad4ada05fa6466a0505674ac61
GregPlaya <110426691+GregPlaya@users.noreply.github.com>
2022-09-23 23:01:01 -0700

    Update residential3.js

--- src/pages/residential3.js
+++ src/pages/residential3.js
@@ -88,11 +88,9 @@ const ResidentialPage = () => {
       <section className="full">
         <div className="row bg-orange color-white align-center d-flex align-items-center justify-content-center px-2 res-grid" style={{ minHeight: "50vh" }}>
            <div className="col-12 col-md-3 col-lg-3">
-               <div style={{ width: "90%" }}>
-                <LightBox src="https://raw.githubusercontent.com/GregPlaya/sandbox-solana/main/src/images/sandbox-solana-beach-residential-living-leasing-Unit-Type-A.jpg" alt="Unit Type A">
+                <LightBox src="https://raw.githubusercontent.com/GregPlaya/sandbox-solana/main/src/images/sandbox-solana-beach-residential-living-leasing-Unit-Type-A.jpg" alt="Unit Type A" className="img-fluid">
     <img src="https://raw.githubusercontent.com/GregPlaya/sandbox-solana/main/src/images/sandbox-solana-beach-residential-living-leasing-Unit-Type-A.jpg" alt="Unit Type A" />      
               </LightBox> 
-               </div>
              <div className="short-desc caps font-com font-bold mt-50">
               Unit Type A
              </div>
