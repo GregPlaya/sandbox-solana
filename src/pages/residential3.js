9caf039e75f88e7da2d1c8cc24ef6b8041e0dae8
GregPlaya <110426691+GregPlaya@users.noreply.github.com>
2022-09-23 17:21:41 -0700

    Delete extra stuff

--- src/pages/residential3.js
+++ src/pages/residential3.js
@@ -1,12 +1,6 @@
 import * as React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
-
-import "../styles/residential.css"
-import SubscribeFormRes from "../components/mailchimp-form-res"
-
 const LightBox = ({ children, src, alt, Wrapper = 'div', zIndex = 100 }) => {
  
 	const [isOpen, setIsOpen] = React.useState(false);
@@ -40,13 +34,6 @@ const LightBox = ({ children, src, alt, Wrapper = 'div', zIndex = 100 }) => {
 				: null}
 		</Wrapper>
 	);
-};
-
-const LightboxApp = function () {
-	return (<div>
-	  <h1>Lightbox App</h1>
-    <h2>My Gallery</h2>
-	</div>);
 };
 
 /* Step 3: Export your component so it
