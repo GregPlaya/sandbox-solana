ba8ef57f6ef25266c888c38dc6c28be207cac9a3
GregPlaya <110426691+GregPlaya@users.noreply.github.com>
2022-09-23 21:25:09 -0700

    Update residential3.js

--- src/pages/residential3.js
+++ src/pages/residential3.js
@@ -11,7 +11,6 @@ const ResPage = () => {
         <div>
            <h1>Lightbox App</h1>
          <h2>My Gallery</h2>
-          <StaticImage src="../images/sandbox-solana-beach-residential-living-leasing-Unit-Type-A.jpg" alt=""/>
        </div>
       </section>
     </Layout>
