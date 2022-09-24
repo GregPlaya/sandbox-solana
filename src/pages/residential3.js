38e4914e97d8a117321b3c113aea5c4abe48d8c9
GregPlaya <110426691+GregPlaya@users.noreply.github.com>
2022-09-23 17:37:45 -0700

    Update residential3.js

--- src/pages/residential3.js
+++ src/pages/residential3.js
@@ -1,6 +1,6 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
-const LightBox = ({ children, src, alt, Wrapper = 'div' }) => {
+const LightBox = ({ children, src, alt, Wrapper = 'div', zIndex = 100 }) => {
 	const [isOpen, setIsOpen] = useState(false);
 
 	const toggleIsOpen = () => {
@@ -11,8 +11,23 @@ const LightBox = ({ children, src, alt, Wrapper = 'div' }) => {
 		<Wrapper onClick={toggleIsOpen}>
 			{children}
 			{isOpen ?
-				<div onClick={toggleIsOpen}>
-					<img src={src} alt={alt} />
+				<div onClick={toggleIsOpen} style={{
+					position: 'fixed',
+					top: '0',
+					left: '0',
+					height: '100vh',
+					width: '100vw',
+					backgroundColor: 'rgba(0,0,0,0.7)',
+					cursor: 'pointer',
+					zIndex
+				}}>
+					<img src={src}
+						alt={alt}
+						style={{
+							height: 'auto',
+							width: '100%'
+						}}
+					/>
 				</div>
 				: null}
 		</Wrapper>
