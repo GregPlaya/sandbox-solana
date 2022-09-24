9bb0384aec453073bd6bc62de9d46ea800888fbc
GregPlaya <110426691+GregPlaya@users.noreply.github.com>
2022-09-23 21:15:10 -0700

    Create simple.js

---
+++ src/pages/simple.js
@@ -0,0 +1,20 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+
+const ResPage = () => {
+
+  return (
+    <Layout>      
+      <section className="">
+        <div>
+           <h1>Lightbox App</h1>
+         <h2>My Gallery</h2>
+       </div>
+      </section>
+    </Layout>
+  )
+}
+
+export default ResPage
