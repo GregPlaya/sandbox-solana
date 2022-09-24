e94117e374631045d009fb3cc8793f5dc2bb81b2
GregPlaya <110426691+GregPlaya@users.noreply.github.com>
2022-09-23 21:10:34 -0700

    simple page

--- src/pages/residential3.js
+++ src/pages/residential3.js
@@ -1,10 +1,19 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+
 const ResPage = () => {
 
   return (
-     <div>
-      <h1>Lightbox App</h1>
-    <h2>My Gallery</h2>
-    </div>      
+    <Layout>      
+      <section className="">
+        <div>
+           <h1>Lightbox App</h1>
+         <h2>My Gallery</h2>
+       </div>
+      </section>
+    </Layout>
   )
 }
 
