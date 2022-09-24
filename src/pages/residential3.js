8118bfe20030eed7c973b08387790481e7e9e704
GregPlaya <110426691+GregPlaya@users.noreply.github.com>
2022-09-23 17:31:47 -0700

    Update residential3.js

--- src/pages/residential3.js
+++ src/pages/residential3.js
@@ -1,52 +1,11 @@
-import * as React from "react"
+import React from 'react';
 
-const LightBox = ({ children, src, alt, Wrapper = 'div', zIndex = 100 }) => {
- 
-	const [isOpen, setIsOpen] = React.useState(false);
-
-	const toggleIsOpen = () => {
-		setIsOpen(!isOpen);
-	};
-
+const LightBox = ({children, src, alt, Wrapper = 'div'}) => {
 	return (
-		<Wrapper onClick={toggleIsOpen}>
-			{children}
-			{isOpen ?
-				<div onClick={toggleIsOpen} style={{
-					position: 'fixed',
-					top: '0',
-					left: '0',
-					height: '100vh',
-					width: '100vw',
-					backgroundColor: 'rgba(0,0,0,0.7)',
-					cursor: 'pointer',
-					zIndex
-				}}>
-					<img src={src}
-						alt={alt}
-						style={{
-							height: '100%',
-							width: 'auto'
-						}}
-					/>
-				</div>
-				: null}
+		<Wrapper>
+			
 		</Wrapper>
 	);
 };
 
-const LightboxApp = function () {
-	return (<div>
-	  <h1>Lightbox App</h1>
-    <h2>My Gallery</h2>
-      <LightBox src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png" alt="React Logo">
-    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png" width="100px" height="100px" alt="React Logo" />
-      </LightBox>
-	</div>);
-};
-
-/* Step 3: Export your component so it
-can be used by other parts of your app. */
-export const LightboxApp
-
-ReactDOM.render(<LightboxApp />, document.querySelector("#app"))
+export default LightBox;
