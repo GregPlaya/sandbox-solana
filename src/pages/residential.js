00c816dac4e8c972c5df285948af47980daa16d2
GregPlaya <110426691+GregPlaya@users.noreply.github.com>
2022-09-26 15:01:01 -0700

    Update residential.js

--- src/pages/residential.js
+++ src/pages/residential.js
@@ -44,7 +44,7 @@ const ResidentialPage = () => {
         </div>
       </section>
       <section className="full">
-        <div className="bg-orange color-white align-center d-flex align-items-center justify-content-center" style={{ minHeight: "50vh" }}>
+        <div className="bg-orange color-white align-center d-flex align-items-center justify-content-center res-intro-main" style={{ minHeight: "50vh" }}>
           <div className="desc-box">
              <div className="short-desc caps font-com font-bold mb-20">
               CHIC BEACHSIDE LUXURY RESIDENCES FOR RENT
