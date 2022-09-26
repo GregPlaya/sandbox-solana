27c94959c5cb73706eb09a4c85c263779fa9a6e4
GregPlaya <110426691+GregPlaya@users.noreply.github.com>
2022-09-26 14:11:29 -0700

    Update residential.js

--- src/pages/residential.js
+++ src/pages/residential.js
@@ -70,7 +70,7 @@ const ResidentialPage = () => {
       </section>
       <section className="">
         <div className="d-md-flex">
-          <div className="col-12 col-md-8 d-flex align-items-center justify-content-center">
+          <div className="col-12 col-md-8 align-items-center justify-content-center">
              <div className="headline caps font-com font-bold">
               <h1>PROPERTY FEATURES</h1>
             </div>
